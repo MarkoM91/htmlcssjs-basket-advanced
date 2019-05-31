ad36cac8b9c1b8550af99e77d775ed0d427bf313
MarkoM91 <markomedic91@gmail.com>
2019-05-31 14:25:56 +0200

    commit10

--- basket-advanced.js
+++ basket-advanced.js
@@ -8,43 +8,37 @@ function getRandomChar() {
   var rndInt = getRandom(65, 90);
   var rndChar = String.fromCharCode(rndInt);
 
-
   return(rndChar);
 }
 
 function getRandomId() {
-  var rndId = "";
 
+  var rndId = "";
   for (var i = 0; i < 3; i++) {
 
-  rndId += getRandomChar() + getRandom(0, 9);
+    rndId += getRandomChar() + getRandom(0, 9);
   }
-return rndId
+  return rndId
 }
-
-
 
 function getRandomPlayer() {
 
   var twoPerc = getRandom(0, 100);
   var threePerc = 100 - twoPerc;
   var player = {
-
-  "id" : getRandomId(),
-  "points" : getRandom(0, 30),
-  "bounce" : getRandom(0, 15),
-  "mistake" :getRandom(0, 5),
-  "twoPerc" : twoPerc,
-  "threePerc" : threePerc
-}
-
-return player;
+    "id" : getRandomId(),
+    "points" : getRandom(0, 30),
+    "bounce" : getRandom(0, 15),
+    "mistake" :getRandom(0, 5),
+    "twoPerc" : twoPerc,
+    "threePerc" : threePerc
+  };
+  return player;
 }
 
 function isPresent(player, players) {
 
   var finded = false;
-
   for (var i = 0; i < players.length; i++) {
 
     if (player.id == id) {
@@ -54,50 +48,41 @@ function isPresent(player, players) {
   return  finded;
 }
 
+var players;
 function getPlayerbyId(id, players){
 
-var player;
-   for (var i = 0; i < players.length; i++) {
-     if (players[i].id == id) {
-      player = players[i];
-     }
-   }
-return player;
+  var player;
+  for (var i = 0; i < players.length; i++) {
 
+      if (players[i].id == id) {
+        player = players[i];
+      }
+  }
+  return player;
 }
 
 function getRandomPlayers() {
+
   var players = [];
-
   for (var i = 0; i < 10; i++) {
-     var player = getRandomPlayer();
-    if (!isPresent(player.id, players)) {
+      var player = getRandomPlayer();
 
-        players.push(player);
-    }
-
+      if (!isPresent(player.id, players)) {
+          players.push(player);
+      }
   }
-
   return players;
 }
 
 function updateUI(players) {
-
-
   var option = $('.input-element');
 
-
   for (var i = 0; i < players.length; i++) {
-
     opt=document.createElement("div");
     $(opt).text(players[i].id);
 
-
     option.append(opt);
-
   }
-
-
 }
 
 function clearClick() {
@@ -140,28 +125,23 @@ function playerSelection(players, me) {
  idThreePerc.text(player.threePerc);
 }
 
-
-
-
-
 function init() {
+  getRandomChar();
+  var res = getRandomPlayers();
 
-getRandomChar();
-var res = getRandomPlayers();
-console.log(res);
-var players = getRandomPlayers();
-updateUI(players);
-var clearButton = $('#clear-btn');
-clearButton.click(clearClick);
+  var players = getRandomPlayers();
+  updateUI(players);
 
-var divlist = $('.input-element');
-divlist.click(function() {
+  var clearButton = $('#clear-btn');
+  clearButton.click(clearClick);
 
-var me = $(this);
-playerSelection(players, me);
+  var divlist = $('.input-element > div');
+  divlist.click(function() {
 
-});
+    var me = $(this);
+    playerSelection(players, me);
+
+  });
 }
-
 
 $(document).ready(init);
